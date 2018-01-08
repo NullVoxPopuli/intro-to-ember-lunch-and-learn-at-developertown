ed0b1d5e52213224ecf0d06af6a94cda1c78379c
Preston Sego <LPSego3+dev@gmail.com>
2018-01-07 22:38:50 -0500

    oeu

--- app/pods/components/button-counter/component.js
+++ app/pods/components/button-counter/component.js
@@ -1,22 +1,27 @@
 import Component from '@ember/component';
 import { action, computed } from 'ember-decorators/object'
 
-export default Component.extend({
-  number: 1,
-  power: 2,
+export default class ButtonCounter extends Component {
+  // babel-transform-class-properties
+  // defines class properties in the constructor.
+  // constructor-set properties override the values
+  // passed via the handlebars invocation (set on the prototype).
+  // legacy crap.
+  number = this.number || 1;
+  power = this.power || 2;
 
   @computed('number', 'power')
   result(number, power) {
     return Math.pow(number, power);
-  },
+  }
 
   @action
   increment() {
     this.set('number', this.get('number') + 1)
-  },
+  }
 
   @action
   decrement() {
     this.set('number', this.get('number') - 1)
   }
-})
+}
