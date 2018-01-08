5c7d47e7816a8b54dbf93b22bef1b504d46bea0b
Preston Sego <LPSego3+dev@gmail.com>
2018-01-07 21:57:10 -0500

    well, old syntax works

--- app/pods/components/button-counter/component.js
+++ app/pods/components/button-counter/component.js
@@ -1,22 +1,22 @@
 import Component from '@ember/component';
 import { action, computed } from 'ember-decorators/object'
 
-export default class ButtonCounter extends Component {
-  // number = 1;
-  // power = 2;
+export default Component.extend({
+  number: 1,
+  power: 2,
 
   @computed('number', 'power')
   result(number, power) {
     return Math.pow(number, power);
-  }
+  },
 
   @action
   increment() {
     this.set('number', this.get('number') + 1)
-  }
+  },
 
   @action
   decrement() {
     this.set('number', this.get('number') - 1)
   }
-}
+})
