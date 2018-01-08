f8be902f1cee1e5b5616524694e81f30696b35f3
Preston Sego <LPSego3+dev@gmail.com>
2018-01-08 08:40:31 -0500

    revert to old syntax for the model so that the inspector can know what's going on

--- app/models/event.js
+++ app/models/event.js
@@ -1,6 +1,5 @@
 import DS from 'ember-data';
-import {attr} from 'ember-decorators/data'
 
-export default class Event extends DS.Model {
-  @attr name;
-}
+export default DS.Model.extend({
+  name: DS.attr()
+})
