62a148751283b46bf3a81af6c6f0949d633d64bb
Preston Sego <LPSego3+dev@gmail.com>
2018-01-21 11:16:42 -0500

    no progress

--- src/ui/routes/data-route/route.js
+++ src/ui/routes/data-route/route.js
@@ -1,10 +1,23 @@
+// import Ember from 'ember';
 import Route from '@ember/routing/route';
 import RSVP from 'rsvp';
 
-export default class extends Route {
+// export default class extends Route {
+//   store = Ember.inject.service('store');
+//
+//   model() {
+//     return RSVP.hash({
+//       events: this.get('store').findAll('event')
+//     });
+//   }
+// }
+
+export default Route.extend({
+  // store: Ember.inject.service(),
+
   model() {
     return RSVP.hash({
       events: this.get('store').findAll('event')
     });
   }
-}
+})
