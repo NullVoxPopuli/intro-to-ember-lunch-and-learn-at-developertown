6c7c4cbd32cd8435aac477e4fbc6fe01e18a06e3
Preston Sego <LPSego3+dev@gmail.com>
2018-01-21 11:43:05 -0500

    update test format

--- tests/acceptance/navigation-test.js
+++ tests/acceptance/navigation-test.js
@@ -1,30 +1,34 @@
-import { test } from 'qunit';
-import moduleForAcceptance from 'ember-intro/tests/helpers/module-for-acceptance';
+import { module, test } from 'qunit';
+import { visit, currentURL, click } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
 import { withChai } from 'ember-cli-chai/qunit';
 
-moduleForAcceptance('Acceptance | navigation');
+module('Acceptance | navigation', function(hooks) {
+  setupApplicationTest(hooks);
 
-test('visiting some the non-data-routes', withChai(async function(expect) {
-  await visit('/some-route');
+  test('visiting some the non-data-routes', withChai(async function(expect) {
+    await visit('/some-route');
 
-  expect(currentURL()).to.equal('/some-route');
+    expect(currentURL()).to.equal('/some-route');
 
-  await visit('/some-route/sub-route-a')
+    await visit('/some-route/sub-route-a')
 
-  expect(currentURL()).to.equal('/some-route/sub-route-a');
-}));
+    expect(currentURL()).to.equal('/some-route/sub-route-a');
+  }));
 
-test('visiting some routes via clicking links', withChai(async function(expect) {
-  await visit('/');
+  test('visiting some routes via clicking links', withChai(async function(expect) {
+    await visit('/');
 
-  expect(currentURL()).to.equal('/');
+    expect(currentURL()).to.equal('/');
 
-  await click('a.some-route')
+    await click('a.some-route')
 
-  expect(currentURL()).to.equal('/some-route');
+    expect(currentURL()).to.equal('/some-route');
 
-  await click('a.data-route')
+    await click('a.data-route')
 
-  expect(currentURL()).to.equal('/data-route');
+    expect(currentURL()).to.equal('/data-route');
 
-}));
+  }));
+
+});
